cf97f926dde4f685d255d3ced27899422d17b974
Miguel Mena Correa <miguelmena@outlook.com>
2019-09-09 19:52:42 -0500

    ejercicios directivas de Angular

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,5 +8,102 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+  temas = [
+    {
+      nombre: 'gastronomia',
+      image: 'assets/temas/gastronomia.png'
+    },
+    {
+      nombre: 'playas',
+      image: 'assets/temas/playas.png'
+    },
+    {
+      nombre: 'tendencias',
+      image: 'assets/temas/tendencias.png'
+    },
+    {
+      nombre: 'bares',
+      image: 'assets/temas/bares.jfif'
+    },
+  ];
 
+  gastronomia = [
+    {
+      nombre: 'Bandeja Paisa',
+      origen: 'Medellin',
+      image: 'assets/img/plato8.png'
+    },
+    {
+      nombre: 'Ajiaco',
+      origen: 'Bogotá',
+      image: 'assets/img/ajiaco.png'
+    },
+    {
+      nombre: 'Tamal',
+      origen: 'Tolima',
+      image: 'assets/img/tamal.png'
+    },
+
+  ];
+  playas = [
+    {
+      nombre: 'Playa Blanca',
+      lugar: 'Cartagena',
+      image: 'assets/img/playablanca.jfif'
+    },
+    {
+      nombre: 'Rodadero',
+      lugar: 'Santa Marta',
+      image: 'assets/img/rodadero.jfif'
+    },
+    {
+      nombre: 'Bocagrande',
+      lugar: 'Cartagena',
+      image: 'assets/img/bocagrande.jfif'
+    }
+  ];
+  tendencias = [
+    {
+      nombre: 'Smartphones',
+      descripcion: 'Tecnología',
+      image: 'assets/img/smartphones.jfif'
+    },
+    {
+      nombre: 'Video Juegos',
+      descripcion: 'Geek',
+      image: 'assets/img/lol.jfif'
+    },
+    {
+      nombre: 'Ciclismo',
+      descripcion: 'deportes',
+      image: 'assets/img/ciclismo.jfif'
+    }
+  ];
+  bares = [
+    {
+     nombre: 'MORENA ROOFTOP BAR',
+     ubicacion: 'Cra 13 # 83-19',
+     image: 'assets/img/morena.jpg'
+    },
+    {
+     nombre: 'EL PICÓ',
+     ubicacion: 'Avenida 82 # 12A-23',
+     image: 'assets/img/elpico.jpeg'
+    },
+    {
+     nombre: 'CASA BIZARRA',
+     ubicacion: 'Cra. 5 #70a-7',
+     image: 'assets/img/casabizarra.jpg'
+    },
+  ];
+
+  temaSeleccionado = '';
+
+  mostrartema(tema: string) {
+    this.temaSeleccionado = tema;
+    console.log(this.temaSeleccionado);
+  }
+  reiniciartema(tema: string) {
+    this.temaSeleccionado = tema;
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,38 @@ export class Tab2Page {
 
   constructor() {}
 
+
+  titulo = 'Lugares turísticos';
+  lugares = [
+    {
+      nombre: 'Caño Cristales',
+      departamento: 'Meta',
+      descripcion: 'Ha sido denominado «el río de los dioses», «el río de los siete colores», «el arco iris que se derritió» e incluso «el río más hermoso del mundo»1​ ya que en su fondo se reproducen plantas acuáticas que con la exposición al sol cambian sus colores en un proceso que va desde el verde hasta un rojo intenso, lo que produce la sensación de estar frente a un río de seis colores: rojo, amarillo, verde , negro , azul y rosado principalmente.',
+      image: 'assets/img/caño_cristales.jpg'
+    },
+    {
+      nombre: 'Desierto de la Tatacoa',
+      departamento: 'Huila',
+      descripcion: 'Es la segunda zona árida más extensa de Colombia después de la península de La Guajira, es uno de los escenarios naturales más atractivos de Colombia, de tierra de color ocre y gris con pincelazos del verde de los cactus. El Desierto de la Tatacoa tiene dos colores característicos: ocre en el sector del Cuzco y gris en la zona de Los Hoyos.',
+      image: 'assets/img/desierto_tatacoa.jpg'
+    },
+    {
+      nombre: 'Sierra Nevada',
+      departamento: 'Guajira-Magdalena',
+      descripcion: 'es un sistema montañoso litoral; ubicado al norte de Colombia que constituye por sí mismo un sistema aislado de Los Andes, sobre la costa Caribe de Colombia. Esta se eleva abruptamente desde las costas del Mar Caribe hasta alcanzar picos nevados a una altura de 5.775 msnm, si bien datos provistos por la misión SRTM señalan una altitud menor, cercana a los 5.700 msnm.',
+      image: 'assets/img/sierra_nevada.jfif'
+    },
+    {
+      nombre: 'Cartagena de Indias',
+      departamento: 'Bolivar',
+      descripcion: ' oficialmente Distrito Turístico y Cultural de Cartagena de Indias abreviado Cartagena de Indias, D. T. y C., es la capital del departamento de Bolívar, al norte de Colombia.7​ Fue fundada el 1.° de junio de 1533 por Pedro de Heredia.8​ Desde 1991 Cartagena es un Distrito Turístico y Cultural.9​ La ciudad está localizada a orillas del mar Caribe',
+      image: 'assets/img/ciudad_amurallada.jfif'
+    },
+    {
+      nombre: 'Monserrate',
+      departamento: 'Bogotá',
+      descripcion: 'El cerro de Monserrate es el más conocido de los cerros Orientales de Bogotá. Junto a Guadalupe es uno de los cerros titulares de la ciudad. Monserrate tiene una altitud de 3152 m y se ubica sobre la cordillera oriental.1​Los cerros de Bogotá, de origen sedimentario, tienen por lo menos 16 millones de años de antigüedad, con rocas de edad cretácica pertenecientes al Grupo guadalupe, en lo que se refiere al aspecto geológico.',
+      image: 'assets/img/monserrate.jfif'
+    },
+  ];
 }
